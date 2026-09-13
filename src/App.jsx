b31dd63ea7b21831a5ba3bccd965ed791acb32e8
Rodrigo Fernández <rodrip437@gmail.com>
2026-09-12 23:08:58 -0500

    Primera version

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Tldraw } from '@tldraw/tldraw';
+import '@tldraw/tldraw/tldraw.css';
+
+export default function App() {
+  return (
+    <div style={{ position: 'fixed', inset: 0 }}>
+      <Tldraw persistenceKey="mi-pizarra-personal" />
+    </div>
+  );
+}
