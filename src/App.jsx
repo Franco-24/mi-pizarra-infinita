165cd7176d501e476d18df06767a11568486cb73
Rodrigo Fernández <rodrip437@gmail.com>
2026-09-12 23:43:13 -0500

    Forzar llave limpia de tldraw

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Tldraw } from 'tldraw';
-import 'tldraw/tldraw.css';
+```jsx
+import { Tldraw } from 'tldraw'
+import 'tldraw/tldraw.css'
 
 export default function App() {
   return (
-    <div style={{ position: 'fixed', inset: 0, width: '100vw', height: '100vh' }}>
-      <Tldraw persistenceKey="pizarra-limpia-v2" />
+    <div style={{ position: 'fixed', inset: 0 }}>
+      <Tldraw />
     </div>
-  );
+  )
 }
+```
